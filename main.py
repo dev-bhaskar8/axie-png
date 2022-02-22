caa1293a39db6e2a436bc7d4b9aeea731b781744
dev-bhaskar8 <bhaskar.syamal@gmail.com>
2022-02-23 02:25:58 +0530

    real

--- main.py
+++ main.py
@@ -10,23 +10,14 @@ axie_images = []
 new_axie_images = []
 
 for i in range(3):
-    try:
-        url = f'https://assets.axieinfinity.com/axies/{str(axieIds[i])}/axie/axie-full-transparent.png'
-        response = Image.open(BytesIO(requests.get(url).content))
-        axie_images.append(cv2.cvtColor(np.array(response), cv2.COLOR_RGB2BGR))
-    except:
-        response = Image.open(BytesIO(requests.get('https://assets.axieinfinity.com/axies/2580/axie/axie-full-transparent.png').content))
-        response=cv2.cvtColor(np.zeros_like(response, dtype=np.uint8), cv2.COLOR_RGB2BGR)
-        axie_images.append(response)
+    url = f'https://assets.axieinfinity.com/axies/{str(axieIds[i])}/axie/axie-full-transparent.png'
+    response = Image.open(BytesIO(requests.get(url).content))
+    axie_images.append(cv2.cvtColor(np.array(response), cv2.COLOR_RGB2BGR))
+
 for i in range(3):
-    try:
-        url = f'https://assets.axieinfinity.com/axies/{str(newAxieIds[i])}/axie/axie-full-transparent.png'
-        response = Image.open(BytesIO(requests.get(url).content))
-        new_axie_images.append(cv2.cvtColor(np.array(response), cv2.COLOR_RGB2BGR))
-    except:
-        response = Image.open(BytesIO(requests.get('https://assets.axieinfinity.com/axies/2580/axie/axie-full-transparent.png').content))
-        response=cv2.cvtColor(np.zeros_like(response, dtype=np.uint8), cv2.COLOR_RGB2BGR)
-        axie_images.append(response)
+    url = f'https://assets.axieinfinity.com/axies/{str(newAxieIds[i])}/axie/axie-full-transparent.png'
+    response = Image.open(BytesIO(requests.get(url).content))
+    new_axie_images.append(cv2.cvtColor(np.array(response), cv2.COLOR_RGB2BGR))
 
 axie_stack1 = cv2.hconcat([axie for axie in axie_images])
 axie_stack2 = cv2.hconcat([axie for axie in new_axie_images])
